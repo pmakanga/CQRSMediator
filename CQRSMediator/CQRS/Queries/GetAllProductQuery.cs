4b9c02d2bea40d0263b7349dfb2e0d2d34223e31
pmakanga <pmakanga@gmail.com>
2021-09-23 12:06:43 +0300

    Add project files.

---
+++ CQRSMediator/CQRS/Queries/GetAllProductQuery.cs
@@ -0,0 +1,29 @@
+﻿using CQRSMediator.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRSMediator.CQRS.Queries
+{
+    public class GetAllProductQuery : IRequest<IEnumerable<Product>>
+    {
+        public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQuery, IEnumerable<Product>>
+        {
+            private readonly ProductContext context;
+            public GetAllProductQueryHandler(ProductContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<IEnumerable<Product>> Handle(GetAllProductQuery query, CancellationToken cancellationToken)
+            {
+                var productList = await context.Products.ToListAsync();
+                return productList;
+            }
+        }
+    }
+}
---
+++ CQRSMediator/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using CQRSMediator.CQRS.Commands;
+using CQRSMediator.CQRS.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSMediator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+       
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            return Ok(await mediator.Send(command));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await mediator.Send(new GetAllProductQuery()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await mediator.Send(new GetProductByIdQuery { Id = id }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateProductCommand command)
+        {
+            command.Id = id;
+            return Ok(await mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await mediator.Publish(new Notifications.DeleteProductNotification { ProductId = id }); //publish deleted productId to Email and SMS handlers
+            return Ok(await mediator.Send(new DeleteProductByIdCommand { Id = id }));
+        }
+    }
+}
